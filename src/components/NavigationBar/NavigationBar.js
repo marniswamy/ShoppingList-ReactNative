62b2466b02daab054def89a857fae0257f607dab
Marni Swamy <ayyappa.marni@hotmail.com>
2019-08-10 23:53:06 +0200

    Modal component added

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 /**
@@ -7,19 +7,48 @@ import Icon from "react-native-vector-icons/MaterialIcons";
  *
  * @returns {*} a React Stateless Functional Component
  */
-export const NavigationBar = () => (
-  <View style={styles.navBar}>
-    <Text>Welcome User!</Text>
-    <View style={styles.rightNav}>
-      <TouchableOpacity>
-        <Icon style={styles.navItem} name="search" size={25} />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Icon style={styles.navItem} name="add" size={25} />
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+export class NavigationBar extends React.Component {
+  state = {
+    modalVisible: false
+  };
+
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
+  render() {
+    return (
+      <View style={styles.navBar}>
+        <Text>Welcome User!</Text>
+        <View style={styles.rightNav}>
+          <TouchableOpacity>
+            <Icon style={styles.navItem} name="search" size={25} />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.setModalVisible(true)}
+          >
+            <Icon style={styles.navItem} name="add" size={25} />
+          </TouchableOpacity>
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={this.state.modalVisible}
+          >
+            <View style={styles.modal}>
+              <Text>Hello World!</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   navBar: {
@@ -37,5 +66,8 @@ const styles = StyleSheet.create({
   },
   navItem: {
     marginLeft: 25
+  },
+  modal: {
+    backgroundColor: "lightgrey"
   }
 });
